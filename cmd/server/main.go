d1415b18a80834d4ca055868f469f124eb0b032a
Nicolas Garza <nicolas.garza@gmail.com>
2024-06-24 22:34:03 -0500

    handler boilerplate and initialize servers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"log"
 	// "net"
-	// "google.golang.org/grpc"
-	// pb "chat_app/chat"
 	"chat_app/config"
 	"chat_app/internal/chat"
 	"chat_app/internal/logger"
 	"chat_app/internal/ratelimit"
+	pb "chat_app/pb"
+	"net"
 	"time"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
 
 	"golang.org/x/time/rate"
 )
@@ -27,13 +30,25 @@ func main() {
 
 	logger.Log.Info("Application started")
 
-	// init rate limiter
 	rateLimiter := ratelimit.NewRateLimiter(rate.Every(time.Second), 100)
 
 	// init chatserver
 	chatServer := chat.NewChatServer(rateLimiter)
 
 	// initialize grpc server
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		logger.Log.Fatal("Failed to listen", zap.Error(err))
+	}
+
+	grpcServer := grpc.NewServer()
+
 	// register chatservice
+	pb.RegisterChatServiceServer(grpcServer, chatServer)
+
 	// start listening
+	logger.Log.Info("Staarting gRPC server", zap.String("Address", lis.Addr().String()))
+	if err := grpcServer.Serve(lis); err != nil {
+		logger.Log.Fatal("Failed to serve", zap.Error(err))
+	}
 }
--- internal/chat/service.go
+++ internal/chat/service.go
@@ -1,9 +1,15 @@
 package chat
 
 import (
+	"chat_app/internal/logger"
 	"chat_app/internal/ratelimit"
 	pb "chat_app/pb"
 	"context"
+	"time"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	// "chat_app/internal/storage"
 )
 
@@ -14,14 +20,38 @@ type ChatServer struct {
 }
 
 func NewChatServer(rateLimiter *ratelimit.RateLimiter) *ChatServer {
-	// init new chat server
+	return &ChatServer{
+		rateLimiter: rateLimiter,
+	}
 }
 
 func (s *ChatServer) SendMessage(ctx context.Context, msg *pb.ChatMessage) (*pb.Empty, error) {
-	// impl sending a message
-	// return &pb.Empty{}, nil if successful
+	if !s.rateLimiter.Allow() {
+		return nil, status.Errorf(codes.ResourceExhausted, "Rate limit exceeded")
+	}
+
+	// TODO: Add logic to process and store the SendMessage
+	// EX: save to redis
+
+	logger.Log.Info("Message sent", zap.String("user", msg.User), zap.String("message", msg.Message))
+	return &pb.Empty{}, nil
 }
 
 func (s *ChatServer) StreamMessages(empty *pb.Empty, stream pb.ChatService_StreamMessagesServer) error {
-	// Implement streaming messages
+	// TODO: Implement logic to stream messages
+	// Set up a goroutime to listen for new messages and send them over the stream
+
+	for {
+		// EX: send a message every 5 seconds
+		time.Sleep(5 * time.Second)
+		msg := &pb.ChatMessage{
+			User:      "System",
+			Message:   "This is a test message",
+			Timestamp: time.Now().Unix(),
+		}
+		if err := stream.Send(msg); err != nil {
+			return err
+		}
+
+	}
 }
