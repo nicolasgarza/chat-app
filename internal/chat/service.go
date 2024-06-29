90d439445027b0cd66b30d6db483ce72a95059be
Nicolas Garza <nicolas.garza@gmail.com>
2024-06-28 22:12:17 -0500

    move some stuff to handlers

--- internal/chat/service.go
+++ internal/chat/service.go
@@ -7,12 +7,10 @@ import (
 	pb "chat_app/pb"
 	"context"
 	"encoding/json"
-	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/golang-jwt/jwt/v5"
 	"go.uber.org/zap"
-	"golang.org/x/crypto/bcrypt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
@@ -83,79 +81,11 @@ func (s *ChatServer) StreamMessages(empty *pb.Empty, stream pb.ChatService_Strea
 }
 
 func (s *ChatServer) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.AuthResponse, error) {
-	// check if username already exists and hash password
-	_, err := storage.GetUser(s.redisClient, req.Username)
-	if err == nil {
-		// user already exists
-		return nil, status.Errorf(codes.AlreadyExists, "Username already exists")
-	} else if err != redis.Nil {
-		// unexpected error
-		logger.Log.Error("Error checking user existence:", zap.Error(err))
-		return nil, status.Errorf(codes.Internal, "Error checking user existence")
-	}
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
-	if err != nil {
-		logger.Log.Error("Error hashing password: ", zap.Error(err))
-		return nil, status.Errorf(codes.Internal, "Failed to hash password")
-	}
-
-	err = storage.SaveUser(s.redisClient, req.Username, string(hashedPassword))
-	if err != nil {
-		logger.Log.Error("Error saving user:", zap.Error(err))
-		return nil, status.Errorf(codes.Internal, "Failed to save user: %v", err)
-	}
-
-	// store username and hashed password in db
-	// Generate JWT token
-	token, err := generateToken(req.Username)
-	if err != nil {
-		logger.Log.Error("Error generating token:", zap.Error(err))
-		return nil, status.Errorf(codes.Internal, "Failed to generate token")
-	}
-
-	err = storage.SaveToken(s.redisClient, req.Username, token, 24*time.Hour)
-	if err != nil {
-		logger.Log.Error("Error saving token:", zap.Error(err))
-		return nil, status.Errorf(codes.Internal, "Failed to save token: %v", err)
-	}
-	return &pb.AuthResponse{Token: token}, nil
+	return HandleRegister(s.redisClient, req)
 }
 
 func (s *ChatServer) Login(ctx context.Context, req *pb.LoginRequest) (*pb.AuthResponse, error) {
-	// Retrieve hashed password for username from database
-	hashedPassword, err := storage.GetUser(s.redisClient, req.Username)
-	if err != nil {
-		return nil, status.Errorf(codes.NotFound, "User not found: %v", err)
-	}
-
-	// Compare passwords
-	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(req.Password))
-	if err != nil {
-		return nil, status.Errorf(codes.Unauthenticated, "Invalid credentials")
-	}
-
-	// Generate JWT token
-	token, err := generateToken(req.Username)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to generate token")
-	}
-
-	// save token to redis
-	err = storage.SaveToken(s.redisClient, req.Username, token, 24*time.Hour)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Failed to save token: %v", err)
-	}
-
-	return &pb.AuthResponse{Token: token}, nil
-}
-
-func generateToken(username string) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": username,
-		"exp":      time.Now().Add(time.Hour * 24).Unix(),
-	})
-	return token.SignedString([]byte("dogdogdog"))
+	return HandleLogin(s.redisClient, req)
 }
 
 func (s *ChatServer) AuthInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
